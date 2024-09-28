11619c2edf491f6e4e902adc27ceb1154e929aa1
Irfan0209 <irfanardiansyah0209@gmail.com>
2024-09-28 19:20:17 +0700

    Program pertama

---
+++ jam_TPQ.ino
@@ -0,0 +1,19 @@
+/*===============================
+
+#feature
+-baris atas jam & tulisan berjalan hari nasional+hari jawa,tanggal bulan tahun hijriah dan nasional
+-barisan bawah tulisan berjalan "TPQ"
+
+Program V1.0
+
+==================================*/
+
+void setup() {
+  
+
+}
+
+void loop() {
+  
+
+}
